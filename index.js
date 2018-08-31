afe8f672f8cffce3608d59c5bb11b5dbe58805d6
Dallas <dallasbille@gmail.com>
2018-08-31 22:01:44 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,15 @@
+var recipes= {
+  prop: 1
+
+  }
+function updateObjectWithKeyAndValue(object, key, value){
+ return Object.assign({prop:1},{prop2:2})
+}
+function destructivelyUpdateObjectWithKeyAndValue(object,key,value){
+ object.prop2= 2
+ return object
+}
+function deleteFromObjectByKey(object,key){
+  var newObject= object
+  
+  }
