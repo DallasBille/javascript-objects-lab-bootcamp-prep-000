c9e31e449e7c608d67ff834aab5a59b6d66a5772
Dallas <dallasbille@gmail.com>
2018-08-31 22:07:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,4 +14,6 @@ function deleteFromObjectByKey(object,key){
   delete newObject[key]
   return newObject.prop= 1
   }
-  function dustructivelyDeleteFromObjectByKey(object,key)
+  function dustructivelyDeleteFromObjectByKey(object,key){
+    
+  }
